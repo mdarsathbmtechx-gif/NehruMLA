28dc422b6ba41d3faa23a0151037b9f96c89289f
mdarsathbmtechx-gif <mdarsath.bmtechx@gmail.com>
2025-08-26 15:04:04 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./Pages/Home";
 import Join from "./Pages/Join";
 import Aboutsection from "./components/About";
+import Home from "./Pages/Home";
 
 export default function App() {
   return (
